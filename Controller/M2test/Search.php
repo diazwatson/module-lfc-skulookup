38ae17fa2db2966ffeda4a3836790d40de54d0d8
Raul E Watson <diazwatson@gmail.com>
2023-03-07 12:49:45 +0000

    LFCR-100: Handle requests sent by the search form

--- Controller/M2test/Search.php
+++ Controller/M2test/Search.php
@@ -4,14 +4,56 @@ declare(strict_types=1);
 
 namespace LFC\SkuLookUp\Controller\M2test;
 
-
+use Magento\Catalog\Api\ProductRepositoryInterface;
+use Magento\Catalog\Model\Product\Visibility;
 use Magento\Framework\App\Action\HttpPostActionInterface;
+use Magento\Framework\App\RequestInterface;
+use Magento\Framework\App\ResponseInterface;
+use Magento\Framework\Controller\Result\Redirect;
+use Magento\Framework\Controller\ResultFactory;
+use Magento\Framework\Controller\ResultInterface;
+use Magento\Framework\Exception\LocalizedException;
+use Magento\Framework\Exception\NoSuchEntityException;
+use Magento\Framework\Message\ManagerInterface;
 
 class Search implements HttpPostActionInterface
 {
+    private ResultFactory $resultFactory;
+    private RequestInterface $request;
+    private ProductRepositoryInterface $productRepository;
+    private ManagerInterface $messageManager;
 
+    public function __construct(
+        RequestInterface $request,
+        ResultFactory $resultFactory,
+        ProductRepositoryInterface $productRepository,
+        ManagerInterface $messageManager
+    ) {
+        $this->request = $request;
+        $this->resultFactory = $resultFactory;
+        $this->productRepository = $productRepository;
+        $this->messageManager = $messageManager;
+    }
+
+    /**
+     * @return ResponseInterface|Redirect|(Redirect&ResultInterface)|ResultInterface
+     */
     public function execute()
     {
-        // TODO: Implement execute() method.
+        $redirect = $this->resultFactory->create(ResultFactory::TYPE_REDIRECT);
+        $params = $this->request->getParams();
+        try {
+            $product = $this->productRepository->get($params['sku']);
+            if ((int)$product->getVisibility() !== Visibility::VISIBILITY_NOT_VISIBLE) {
+                $redirect->setUrl($product->getProductUrl());
+            } else {
+                throw new LocalizedException(__('The requested product is not visible individually'));
+            }
+        } catch (NoSuchEntityException|LocalizedException $e) {
+            $this->messageManager->addNoticeMessage($e->getMessage());
+            $redirect->setUrl('/lfcretail/m2test');
+        }
+
+        return $redirect;
     }
 }
