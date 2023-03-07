e066f72b1edcffd115e1b5182b2e41bcb49ee817
Raul E Watson <diazwatson@gmail.com>
2023-03-07 16:21:16 +0000

    LFCR-100: Stored log search events

--- Controller/M2test/Search.php
+++ Controller/M2test/Search.php
@@ -22,37 +22,54 @@ class Search implements HttpPostActionInterface
     private RequestInterface $request;
     private ProductRepositoryInterface $productRepository;
     private ManagerInterface $messageManager;
+    private \LFC\SkuLookUp\Api\ProductSearchRepositoryInterface $productSearchRepository;
+    private \LFC\SkuLookUp\Api\Data\ProductSearchInterfaceFactory $productSearchFactory;
 
     public function __construct(
         RequestInterface $request,
         ResultFactory $resultFactory,
         ProductRepositoryInterface $productRepository,
-        ManagerInterface $messageManager
+        ManagerInterface $messageManager,
+        \LFC\SkuLookUp\Api\ProductSearchRepositoryInterface $productSearchRepository,
+        \LFC\SkuLookUp\Api\Data\ProductSearchInterfaceFactory $productSearchFactory
     ) {
         $this->request = $request;
         $this->resultFactory = $resultFactory;
         $this->productRepository = $productRepository;
         $this->messageManager = $messageManager;
+        $this->productSearchRepository = $productSearchRepository;
+        $this->productSearchFactory = $productSearchFactory;
     }
 
     /**
      * @return ResponseInterface|Redirect|(Redirect&ResultInterface)|ResultInterface
+     * @throws LocalizedException
      */
     public function execute()
     {
+        /** @var \LFC\SkuLookUp\Model\ProductSearch $productSearch */
+        $productSearch = $this->productSearchFactory->create();
+
         $redirect = $this->resultFactory->create(ResultFactory::TYPE_REDIRECT);
         $params = $this->request->getParams();
+
         try {
+            $productSearch->setSku($params['sku']);
             $product = $this->productRepository->get($params['sku']);
+
             if ((int)$product->getVisibility() !== Visibility::VISIBILITY_NOT_VISIBLE) {
                 $redirect->setUrl($product->getProductUrl());
+                $productSearch->setResult('Success');
             } else {
                 throw new LocalizedException(__('The requested product is not visible individually'));
             }
         } catch (NoSuchEntityException|LocalizedException $e) {
+            $productSearch->setResult('Error: ' . $e->getMessage());
             $this->messageManager->addNoticeMessage($e->getMessage());
             $redirect->setUrl('/lfcretail/m2test');
         }
+
+        $this->productSearchRepository->save($productSearch);
 
         return $redirect;
     }
